45d3b25c62bf9f26048cb7ae9bf1574d875bdafd
SliverChen <75901408+SliverChen@users.noreply.github.com>
2020-12-23 23:26:25 +0800

    leetcode103——通过版本(内存消耗11.2M，耗时8ms)

--- LeetCode/二叉树锯齿层次遍历2.cpp
+++ LeetCode/二叉树锯齿层次遍历2.cpp
@@ -10,29 +10,29 @@
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-	/*����һ����Ž����vector<vector<int>>���ͱ���res 
-	�ж�root�Ƿ�Ϊ�գ����Ϊ����ֱ�� ����res
-	�������ڴ�Žڵ������˫�˶���deque<int> d1
-	�������ڴ�Žڵ��˫�˶���deque<TreeNode*> d2
-	��ʼ������Ų���1�ͽ��root���������Ϊ1
-	����ѭ����(����d1�Ƿ�Ϊ��)
-	  �ж���ȵ���ż��
-	   ���Ϊ����
-	     ������ѭ���� 
-	       ������ʱ����nowD��node��ֵ��d1��d2��ǰ�˻�ȡ�ò����ͽڵ� 
-	       �����ȡ�Ĳ�����ò�������ͬ����ֹͣ��ѭ�������ڱ����ò�����е㣩 
-		   ����ȡ�Ľڵ��Ӧ��ֵ��ŵ�out�� 
-	       ��d1��d2��ǰ��ɾ�������ڽ�����һ�ڵ�ı�����
-		   �Ƚ���ڵ�ѹ��������
-		   �ٽ��ҽڵ�ѹ��������
-	   ���Ϊż��
-	   	 ������ѭ����
-			��������������
-			�����ѹ�����еĲ���ʱ��ѡ���Ƚ��ҽڵ�ѹ�����У��ٽ���ڵ�ѹ������
-		��out��ŵ�res�У�����out���
-		��ʱ�������+1 
+	/*创建一个存放结果的vector<vector<int>>类型变量res 
+	判断root是否为空，如果为空则直接 返回res
+	创建用于存放节点层数的双端队列deque<int> d1
+	创建用于存放节点的双端队列deque<TreeNode*> d2
+	初始化：存放层数1和结点root，设置深度为1
+	进入循环：(根据d1是否为空)
+	  判断深度的奇偶性
+	   如果为奇数
+	     进入死循环： 
+	       创建临时变量nowD和node赋值从d1和d2的前端获取该层数和节点 
+	       如果获取的层数与该层数不相同，则停止死循环（用于遍历该层的所有点） 
+		   将获取的节点对应的值存放到out中 
+	       将d1和d2的前端删除（用于进入下一节点的遍历）
+		   先将左节点压进队列中
+		   再将右节点压进队列中
+	   如果为偶数
+	   	 进入死循环：
+			与奇数做法类似
+			最后在压进队列的操作时，选择先将右节点压进队列，再将左节点压进队列
+		将out存放到res中，并将out清空
+		此时深度往下+1 
 	 
-	 ��󷵻�res 
+	 最后返回res 
 	 
 	 */
 	 
@@ -42,7 +42,7 @@ public:
 	 deque<int> depth;
 	 deque<TreeNode*> node;
 	 
-	 int Depth = 1;  //��ʼ�� 
+	 int Depth = 1;  //初始化 
 	 depth.push_back(1);
 	 node.push_back(root);
 	 
