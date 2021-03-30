6dc1b92b32427b0d87725746cc43b6915fcb3021
SliverChen <SliverCChen@163.com>
2021-03-31 00:28:21 +0800

    Update practice 2021/3/31 0:28

--- PraticeCode/pratice.cpp
+++ PraticeCode/pratice.cpp
@@ -1,9 +1,22 @@
+/*
+Topic: 左值与右值的本质区别->能够对表达式用&符进行取址
+*/
+
 #include<iostream>
 using namespace std;
 
 int main()
 {
-    cout<<"Hello world"<<endl;
-    system("pause");
-    return 0;
+   int x = 0;
+   cout << (&x) << endl;
+   cout << (&++x) << endl;
+   cout << (&x) << endl;
+   x++;
+   //cout<<(&x++)<<endl; error
+   /*
+   原因：++x的结果为左值，该表达式修改自身并返回自身
+        x++的结果为右值，该表达式先创建一个临时对象，为其赋值，再返回临时对象
+   */
+   system("pause");
+   return 0;
 }
