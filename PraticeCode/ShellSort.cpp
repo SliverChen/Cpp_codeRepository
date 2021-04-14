cee8938ae5548b89d5b2aa9d0d9effdf72dc9260
SliverChen <SliverCChen@163.com>
2021-04-14 20:28:16 +0800

    implement ShellSort and BucketSort(unfinish) at PC

--- PraticeCode/ShellSort.cpp
+++ PraticeCode/ShellSort.cpp
@@ -6,13 +6,56 @@
 #include <vector>
 using namespace std;
 
+void InsertSort(vector<int> &arr)
+{
+    int j;
+    bool flag;
+    for (int i = 1; i < arr.size(); i++)
+    {
+        if (arr[i - 1] > arr[i])
+        {
+            flag = true;
+            j = i - 1;
+            swap(arr[i - 1], arr[i]);
+            while (flag && j - 1 >= 0)
+            {
+                if (arr[j - 1] > arr[j])
+                {
+                    swap(arr[j - 1], arr[j]);
+                    j = j - 1;
+                }
+                else
+                    flag = false;
+            }
+        }
+    }
+}
+
 void ShellSort(vector<int> &arr)
 {
+    //从小到大
+    int delta = arr.size() / 2;
+    while (delta > 0)
+    {
+        if (delta == 1)
+        {
+            InsertSort(arr);
+            break;
+        }
+        for (int i = 0; i < arr.size() - delta; i++)
+        {
+            if (arr[i] > arr[i + delta])
+            {
+                swap(arr[i], arr[i + delta]);
+            }
+        }
+        delta = delta / 2;
+    }
 }
 
 int main()
 {
-    vector<int> arr = {5, 2, 2, 3, 8};
+    vector<int> arr = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
     printf("the previous array is as follows:\n");
     for (auto value : arr)
     {
--- PraticeCode/TongSort.cpp
+++ PraticeCode/TongSort.cpp
@@ -1,37 +1,102 @@
 /*
-    Topic: Tong Sort
+    Topic: Bucket Sort
 */
 
+#include <algorithm>
+#include <cmath>
 #include <cstring>
 #include <iostream>
+#include <list>
 #include <vector>
 using namespace std;
 
-void TongSort(vector<int> &arr)
+int getMaxLength(const vector<int> &arr)
 {
+    int maxLength;
+    int maxElem = INT_MIN;
+    for (auto value : arr)
+    {
+        if (value > maxElem)
+            maxElem = value;
+    }
+    int tempLength = 1, times = 1;
+    while (true)
+    {
+        if (maxElem % (int)pow(10, times) != maxElem)
+        {
+            maxLength = times + 1;
+            times++;
+        }
+        else
+            break;
+    }
+    return maxLength;
+}
+
+int getNumberofDigits(int element, int NumberofDigits)
+{
+    if (NumberofDigits == 0)
+    {
+        return element % 10;
+    }
+    else
+    {
+        int tempNum = element / (int)pow(10, NumberofDigits);
+        return tempNum % 10;
+    }
+}
+
+void BucketSort(vector<int> &arr)
+{
+    vector<vector<int>> bucket(10);
+    int CircleTimes = getMaxLength(arr);
+    int curNum;
+    vector<int> tempArr;
+    for (int time = 0; time < CircleTimes; time++)
+    {
+        //建立映射,将值存放到映射对应的桶中
+        for (auto value : arr)
+        {
+            curNum = getNumberofDigits(value, time);
+            bucket[curNum].push_back(value);
+        }
+
+        //从编号0-9依次将桶中的数据存放到tempArr中
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < bucket[i].size(); j++)
+            {
+                tempArr.push_back(bucket[i][j]);
+            }
+        }
+        //将原来的arr覆盖成tempArr
+        arr = tempArr;
+
+        //清空tempArr和bucket用于下一次的循环
+        tempArr.clear();
+        for (auto array : bucket)
+        {
+            array.clear();
+        }
+    }
 }
 
 int main()
 {
-    //引入：随机输入多个0-1000之间的整数，并将它们按从小到大进行排序
-    int nums[1001];
-    memset(nums, 0, sizeof(nums));
-    int num;
-    while (cin >> num)
+    vector<int> arr = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+    printf("the previous array is as follows:\n");
+    for (auto value : arr)
     {
-        nums[num]++;
-        if (cin.get() == '\n')
-            break;
+        printf("%d ", value);
     }
-    int count;
-    for (int i = 0; i < 1001; i++)
+    printf("\n");
+
+    BucketSort(arr);
+
+    printf("after Tong Sort, the array is as follows:\n");
+    for (auto value : arr)
     {
-        count = 0;
-        while (nums[i] - count != 0)
-        {
-            printf("%d ", i);
-            count++;
-        }
+        printf("%d ", value);
     }
     printf("\n");
     system("pause");
