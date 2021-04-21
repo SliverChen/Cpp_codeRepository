58f0ec2d4f2be304ef20a16c3cf2a0b2d8199d5d
SliverChen <SliverCChen@163.com>
2021-04-21 23:50:43 +0800

    DoublyLinkedList((untest)

--- PraticeCode/PrivateMemberAccess.cpp
+++ PraticeCode/PrivateMemberAccess.cpp
@@ -1,38 +1,31 @@
 /*
     Topic:  How can we access the private member from one class to another class
     Explanation:
-        there are three ways to achieve:
-            1) while in the class that will be accessed, add friend xxx(the class u want to access it) in this class
+    
+            while in the class that will be accessed, add friend xxx(the class u want to access it) in this class
 
                 in this way, if the accessed class is defined previously compared to the class that wants to access
                 before the accessed class, we should defined that xxx is a class type
 
-                the example is as follows example one.
-
-
-            2) make the class that will be accessed into the class u want to access as private member
-
-                this way is only suitble for the accessed class is only correlated with this class
-
-                the example is as follows example two
+                the example is as follows example
 */
 
 #include <iostream>
 using namespace std;
 
-class WantAccess_Ex1;
+class WantAccess;
 
-class BeAccessed_Ex1
+class BeAccessed
 {
 private:
     void print1()
     {
         printf("the beAccessed_ex1 has been called\n");
     }
-    friend WantAccess_Ex1;
+    friend WantAccess;
 };
 
-class WantAccess_Ex1
+class WantAccess
 {
 public:
     void print1()
@@ -42,26 +35,13 @@ public:
     }
 
 private:
-    BeAccessed_Ex1 ex1;
+    BeAccessed ex1;
 };
 
-class WantAccess_Ex2
+int main()
 {
-public:
-    void print2()
-    {
-        printf("the WantAccess_Ex2 has been called\n");
-        ex2.print2();
-    }
-
-private:
-    class BeAccessed_Ex2
-    {
-    private:
-        void print2()
-        {
-            printf("the BeAccessed_Ex2 has been called\n");
-        }
-    };
-    BeAccessed_Ex2 ex2;
-};
+    WantAccess ex1;
+    ex1.print1();
+    system("pause");
+    return 0;
+}
--- PraticeCode/myDoublyLinkedList.cpp
+++ PraticeCode/myDoublyLinkedList.cpp
@@ -5,20 +5,159 @@
 */
 
 template <class T>
+class DoublyList;
+
+template <class T>
 class DoubleListNode
 {
 public:
-    DoubleListNode();
+    DoubleListNode(const T &elem);
     ~DoubleListNode();
 
 private:
     T val;
     DoubleListNode *next;
     DoubleListNode *pre;
-    friend class DoublyList;
+    void clear(DoubleListNode *&t);
+    friend DoublyList<T>;
 };
 
 template <class T>
 class DoublyList
 {
+public:
+    typedef DoubleListNode<T> node_t;
+
+public:
+    DoublyList();
+    ~DoublyList();
+    void Insert(const T &elem);
+    void Remove(const T &elem);
+    void InsertHead(const T &elem);
+    bool Contains(const T &elem);
+
+private:
+    DoubleListNode *head;
 };
+
+/***********************DoubleListNode member achieve***************************/
+
+template <class T>
+DoubleListNode<T>::DoubleListNode(const T &elem)
+{
+    val = elem;
+    next = nullptr;
+    pre = nullptr;
+}
+
+template <class T>
+DoubleListNode<T>::~DoubleListNode()
+{
+    delete this;
+}
+
+template <class T>
+void DoubleListNode<T>::clear(DoubleListNode *&t)
+{
+    if (t == nullptr)
+        return;
+    clear(t->next);
+    delete t;
+}
+
+/**********************DoublyList member achieve**************************/
+
+template <class T>
+DoublyList<T>::DoublyList()
+{
+    head = nullptr;
+}
+
+template <class T>
+DoublyList<T>::~DoublyList()
+{
+    head.clear(head);
+}
+
+template <class T>
+void DoublyList<T>::Insert(const T &elem)
+{
+    if (head == nullptr)
+        head->val = elem;
+    else
+    {
+        node_t *curNode = head;
+        while (curNode->next)
+        {
+            curNode = curNode->next;
+        }
+        node_t *newNode = new node_t(elem);
+        curNode->next = newNode;
+        newNode->pre = curNode;
+    }
+}
+
+template <class T>
+void DoublyList<T>::InsertHead(const T &elem)
+{
+    if (head == nullptr)
+        head->val = elem;
+    else
+    {
+        node_t *newNode = new node_t(elem);
+        newNode->next = head;
+        head->pre = newNode;
+        head = newNode;
+    }
+}
+
+template <class T>
+void DoublyList<T>::Remove(const T &elem)
+{
+    if (head == nullptr)
+        return;
+    if (head->val == elem)
+    {
+        node_t *nextnode = head->next;
+        head = nextnode;
+        head->pre = nullptr;
+    }
+    else
+    {
+        node_t *curNode = head;
+        node_t *preNode = nullptr;
+        while (curNode->next)
+        {
+            if (curNode->val == elem)
+            {
+                node_t *oldNode = curNode;
+                preNode->next = oldNode->next;
+                oldNode->next->pre = preNode;
+                delete oldNode;
+                break;
+            }
+            preNode = curNode;
+            curNode = curNode->next;
+        }
+    }
+}
+
+template <class T>
+bool DoublyList<T>::Contains(const T &elem)
+{
+    if (head == nullptr)
+        return false;
+    if (head->val == elem)
+        return true;
+    else
+    {
+        node_t *curNode = head;
+        while (curNode->next)
+        {
+            if (curNode->val == elem)
+                return true;
+            curNode = curNode->next;
+        }
+        return false;
+    }
+}
